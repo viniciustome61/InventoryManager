835843ddf6255546edf553e4375be9a5e790b8fa
Vinicius Dos Santos Tome <viniciussntos011@gmail.com>
2026-06-06 18:13:50 -0300

    chore: setup initial viewcode and folder structure

---
+++ InventoryManager/InventoryManagerTests/InventoryManagerTests.swift
@@ -0,0 +1,17 @@
+//
+//  InventoryManagerTests.swift
+//  InventoryManagerTests
+//
+//  Created by Vinny   on 06/06/26.
+//
+
+import Testing
+@testable import InventoryManager
+
+struct InventoryManagerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
